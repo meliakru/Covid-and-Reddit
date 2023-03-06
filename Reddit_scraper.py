9ccb661a6e6d2583b5a0bebca6c3c8ae9d404f1a
Amil Sharma <sharma.amil2010@gmail.com>
2023-03-06 13:03:24 -0600

    updated limit, and date type which is getting saved

--- Reddit_scraper.py
+++ Reddit_scraper.py
@@ -1,3 +1,4 @@
+
 import csv
 import praw
 from datetime import datetime, timezone
@@ -5,14 +6,14 @@ reddit = praw.Reddit(client_id='OYjYY_r9i9haQK9wkw-3xw',
                      client_secret='BuYIA0kxVC5HOzSIEBPRnieUPj6Q4g',
                      user_agent='redScrapper') 
 
-subredditName = 'depression'
+subredditName = 'mentalhealth'
 subreddit = reddit.subreddit(subredditName)
 
 # hot_posts_jan_2020 = subreddit.search('timestamp:1577836800..1580515200', sort='hot', limit=5)
-start_timestamp = int(datetime(2020, 1, 1, tzinfo=timezone.utc).timestamp())
-end_timestamp = int(datetime(2020, 2, 1, tzinfo=timezone.utc).timestamp())
+start_timestamp = int(datetime(2019, 1, 1, tzinfo=timezone.utc).timestamp())
+end_timestamp = int(datetime(2020, 1, 1, tzinfo=timezone.utc).timestamp())
 
-top_posts = subreddit.top(limit=500)
+top_posts = subreddit.top(limit=5000000)
 
 filtered_posts = []
 
@@ -23,9 +24,9 @@ for post in top_posts:
 data = []
 header = ['subReddit','Title', 'Content', 'Top Comment', 'upvotes','Number of comments', 'date', 'uniquePostID' ]
 
-filtered_posts_5 = filtered_posts[:5]
+# filtered_posts_5 = filtered_posts[:5]
 
-for post in filtered_posts_5:
+for post in filtered_posts:
     title = post.title
     content = post.selftext
     top_comment = post.comments[0]
@@ -41,9 +42,9 @@ for post in filtered_posts_5:
             
     top_comment_body = top_comment.body
     if top_comment.body:
-      data.append([subredditName, title, content, top_comment.body, post.score, len(post.comments),post.created_utc, post.id])
+      data.append([subredditName, title, content, top_comment.body, post.score, len(post.comments),datetime.fromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S'), post.id])
     else:
-      data.append([subredditName, title, content, "", post.score, len(post.comments), post.created_utc, post.id])
+      data.append([subredditName, title, content, "", post.score, len(post.comments), datetime.fromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S'), post.id])
 
 # Date can be converted in readable format from unix datetime using datetime.fromtimestamp(post.created_utc).strftime('%Y-%m-%d %H:%M:%S')
 # write the data to a CSV file
@@ -51,4 +52,3 @@ with open(subredditName+'reddit_data.csv', mode='w') as file:
     writer = csv.writer(file)
     writer.writerow(header)
     writer.writerows(data)
-
