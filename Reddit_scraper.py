6c07f8bddb56f7e639c7bbe58381b25f5b0f83f2
meliakru <meliakrumm@gmail.com>
2023-03-21 12:27:55 -0500

    adding VADER code

---
+++ Reddit_scraper.py
@@ -0,0 +1,3 @@
+import praw
+reddit = praw.Reddit(client_id='zN1tUBJqEDHKsUVt90fdyw', client_secret='905RC5nf5GcNN0QweqdmCueQt5AzEQ', user_agent='Web Scraper')
+all_posts = reddit.subreddit('depression')
